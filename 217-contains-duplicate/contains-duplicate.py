43d207b14e0c46f766462572dbb6cd22adaedee3
mansak23 <154529430+mansak23@users.noreply.github.com>
2025-06-26 18:59:05 +0530

    Time: 191 ms (5.39%) | Memory: 37.3 MB (5.76%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,8 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        nm=set(nums)
-        print(nm)
-        if(len(nums)==len(nm)):
-            return False
-        return True
+        # nm=set(nums)
+        # print(nm)
+        # if(len(nums)==len(nm)):
+        #     return 
+        # return True
+        count=Counter(nums)
+        print(count)
+        for i in count:
+            if(count[i]>1):
+                return True
+        return False
