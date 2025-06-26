50bd24a89d5f5cef0a152ab2000f75669348c0ba
mansak23 <154529430+mansak23@users.noreply.github.com>
2025-06-27 00:32:44 +0530

    Time: 130 ms (5.09%) | Memory: 13.5 MB (0.00%) - LeetSync

---
+++ 2398-check-if-matrix-is-x-matrix/check-if-matrix-is-x-matrix.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def checkXMatrix(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: bool
+        """
+        diag=[]
+        nondiag=[]
+        start=0
+        end=len(grid)-1
+        for i in range (0,end+1):
+            diag.append(grid[i][i])
+        for i in range(end,-1,-1):
+            diag.append(grid[i][abs(i-end)])
+        for i in range(0,end+1):
+            for j in range(0,end+1):
+                if(i!=j and j!=abs(i-end)):
+                    nondiag.append(grid[i][j])
+        setofnondiag=set(nondiag)
+        print(diag)
+        print(setofnondiag)
+        if ((0 not in diag)and(len(setofnondiag)==1 and (0 in setofnondiag))):
+            return True 
+        return False          
