bc41412602d9d0b7d956ee1f5bf3eece1a8483f4
mansak23 <154529430+mansak23@users.noreply.github.com>
2025-06-27 00:36:03 +0530

    Time: 7 ms (74.58%) | Memory: 13.2 MB (1.69%) - LeetSync

--- 2398-check-if-matrix-is-x-matrix/check-if-matrix-is-x-matrix.py
+++ 2398-check-if-matrix-is-x-matrix/check-if-matrix-is-x-matrix.py
@@ -4,21 +4,34 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: bool
         """
-        diag=[]
-        nondiag=[]
-        start=0
-        end=len(grid)-1
-        for i in range (0,end+1):
-            diag.append(grid[i][i])
-        for i in range(end,-1,-1):
-            diag.append(grid[i][abs(i-end)])
-        for i in range(0,end+1):
-            for j in range(0,end+1):
-                if(i!=j and j!=abs(i-end)):
-                    nondiag.append(grid[i][j])
-        setofnondiag=set(nondiag)
-        print(diag)
-        print(setofnondiag)
-        if ((0 not in diag)and(len(setofnondiag)==1 and (0 in setofnondiag))):
-            return True 
-        return False          
+        # MY CODE TOO LENGTHY AND LESS OPTIMAL 
+        # diag=[]
+        # nondiag=[]
+        # start=0
+        # end=len(grid)-1
+        # for i in range (0,end+1):
+        #     diag.append(grid[i][i])
+        # for i in range(end,-1,-1):
+        #     diag.append(grid[i][abs(i-end)])
+        # for i in range(0,end+1):
+        #     for j in range(0,end+1):
+        #         if(i!=j and j!=abs(i-end)):
+        #             nondiag.append(grid[i][j])
+        # setofnondiag=set(nondiag)
+        # print(diag)
+        # print(setofnondiag)
+        # if ((0 not in diag)and(len(setofnondiag)==1 and (0 in setofnondiag))):
+        #     return True 
+        # return False     
+
+        n = len(grid)
+        
+        for i in range(n):
+            for j in range(n):
+                if i == j or i + j == n - 1:
+                    if grid[i][j] == 0:
+                        return False
+                else:
+                    if grid[i][j] != 0:
+                        return False
+        return True     
