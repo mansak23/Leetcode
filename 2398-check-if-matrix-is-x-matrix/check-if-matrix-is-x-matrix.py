c2a045242b67f1bc25b5ed2d301979407fa2eba3
mansak23 <154529430+mansak23@users.noreply.github.com>
2025-06-27 00:36:52 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (35.59%) - LeetSync

--- 2398-check-if-matrix-is-x-matrix/check-if-matrix-is-x-matrix.py
+++ 2398-check-if-matrix-is-x-matrix/check-if-matrix-is-x-matrix.py
@@ -4,7 +4,25 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: bool
         """
-          
+         # MY CODE TOO LENGTHY AND LESS OPTIMAL 
+        # diag=[]
+        # nondiag=[]
+        # start=0
+        # end=len(grid)-1
+        # for i in range (0,end+1):
+        #     diag.append(grid[i][i])
+        # for i in range(end,-1,-1):
+        #     diag.append(grid[i][abs(i-end)])
+        # for i in range(0,end+1):
+        #     for j in range(0,end+1):
+        #         if(i!=j and j!=abs(i-end)):
+        #             nondiag.append(grid[i][j])
+        # setofnondiag=set(nondiag)
+        # print(diag)
+        # print(setofnondiag)
+        # if ((0 not in diag)and(len(setofnondiag)==1 and (0 in setofnondiag))):
+        #     return True 
+        # return False  
 
         n = len(grid)
         
