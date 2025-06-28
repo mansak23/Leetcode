f66779e396a276f9dc50927fe7c8d49c477ab51f
mansak23 <154529430+mansak23@users.noreply.github.com>
2025-06-28 16:24:50 +0530

    Time: 131 ms (6.34%) | Memory: 13.6 MB (94.87%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
@@ -5,13 +5,19 @@ class Solution(object):
         :type target: str
         :rtype: str
         """
-        min="z"
-        flag=0
-        for i in letters:
-            if i>target and i<=min:
-                min=i
-                flag=1
-        if flag==0:
-            return letters[0]
-        return min
+        # min="z"
+        # flag=0
+        # for i in letters:
+        #     if i>target and i<=min:
+        #         min=i
+        #         flag=1
+        # if flag==0:
+        #     return letters[0]
+        # return min
+        sortlet=sorted(letters)
+        print(sortlet)
+        for i in sortlet:
+            if i>target:
+                return i
+        return letters[0]
         
