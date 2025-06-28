3dd2709ab72614016e488fd0808cab5e2e8b22c8
mansak23 <154529430+mansak23@users.noreply.github.com>
2025-06-28 19:42:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (92.47%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.py
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -4,6 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        """
+        JAVA SOLUTION
+        class Solution {
+    public int[] buildArray(int[] nums) {
+        int [] ans=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
+    }"""
         res=[]
         for i in range(0,len(nums)):
             res.append(nums[nums[i]])
