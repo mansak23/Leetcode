22c6bbbdce461f8f6e3e39de766fa93cb27f83d9
mansak23 <154529430+mansak23@users.noreply.github.com>
2025-06-28 19:26:58 +0530

    Time: 47 ms (5.88%) | Memory: 12.6 MB (76.47%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.py
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.py
@@ -5,9 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-
+        # sortednums=sorted(nums)
+        # i=0
+        # while(k!=len(nums)):
+        #     nums.remove(sortednums[i])
+        #     i+=1
+        # return nums
+        # using enumerate
         withindex=list(enumerate(nums))
         sortenum=sorted(withindex,key=lambda x:x[1],reverse=True)[:k]
         finsort=sorted(sortenum,key=lambda x:x[0])
+        print(finsort)
         return [i[1] for i in finsort]
         
