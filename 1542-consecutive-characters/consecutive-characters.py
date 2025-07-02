a6f8fdbfec337f9390fe3d6b51df8d2bb956096e
mansak23 <154529430+mansak23@users.noreply.github.com>
2025-07-02 23:09:27 +0530

    Time: 11 ms (24.22%) | Memory: 17.7 MB (52.15%) - LeetSync

--- 1542-consecutive-characters/consecutive-characters.py
+++ 1542-consecutive-characters/consecutive-characters.py
@@ -1,22 +1,11 @@
 class Solution:
     def maxPower(self, s: str) -> int:
-        # count=0
-        # mx=0
-        # for i in range(1,len(s)):
-        #     if s[i-1]==s[i]:
-        #         count+=1
-        #     mx=max(mx,count)
-        #     if s[i-1]!=s[i]:
-        #         count=0
-        # return mx+1
-        
-        max_len = count = 1
-        
-        for i in range(1, len(s)):
-            if s[i] == s[i-1]:
-                count += 1
-                max_len = max(max_len, count)
-            else:
-                count = 1
-        
-        return max_len
+        count=0
+        mx=0
+        for i in range(1,len(s)):
+            if s[i-1]==s[i]:
+                count+=1
+            mx=max(mx,count)
+            if s[i-1]!=s[i]:
+                count=0
+        return mx+1
