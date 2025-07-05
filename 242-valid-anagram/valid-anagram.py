b568f43cfa10fcadf674255aef8b5b1640a2065b
mansak23 <154529430+mansak23@users.noreply.github.com>
2025-07-05 17:51:10 +0530

    Time: 7 ms (89.53%) | Memory: 18.1 MB (24.83%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,12 +1,5 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        scount=Counter(s)
-        tcount=Counter(t)
-        if scount==tcount:
-            return True
-        return False
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count1=Counter(s)
+        count2=Counter(t)
+        return count1==count2
