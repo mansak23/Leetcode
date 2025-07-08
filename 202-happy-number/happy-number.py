9ed9651a417afb466e8c78b2035ed07f2a80205c
mansak23 <154529430+mansak23@users.noreply.github.com>
2025-07-08 20:16:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (60.91%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -2,7 +2,6 @@ class Solution:
     def isHappy(self, n: int) -> bool:
         s=str(n)
         num=0
-        if n==7:return True
         while len(s)!=1 or s=="7":
             num=0
             for i in range (len(s)):
