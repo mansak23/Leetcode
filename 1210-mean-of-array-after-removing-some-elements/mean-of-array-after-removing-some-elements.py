61110434d3d8d97f40ddb7b711339d8fb2cff555
mansak23 <154529430+mansak23@users.noreply.github.com>
2025-07-10 00:22:53 +0530

    Time: 4 ms (23.26%) | Memory: 17.8 MB (73.17%) - LeetSync

--- 1210-mean-of-array-after-removing-some-elements/mean-of-array-after-removing-some-elements.py
+++ 1210-mean-of-array-after-removing-some-elements/mean-of-array-after-removing-some-elements.py
@@ -1,6 +1,7 @@
 class Solution:
     def trimMean(self, arr: List[int]) -> float:
-        fiveperc=int(len(arr)/20)
+        n=len(arr)
+        fiveperc=int(n/20)
         arr.sort()
         arr=arr[fiveperc:-fiveperc]
         return mean(arr)
