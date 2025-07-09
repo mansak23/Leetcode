df1001ac6babfc5ea38760ab44b04c9c6bfc1022
mansak23 <154529430+mansak23@users.noreply.github.com>
2025-07-09 14:47:22 +0530

    Time: 3 ms (94.89%) | Memory: 18.5 MB (89.99%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,8 +1,12 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        res=[]
-        for i in range(n+1):
-            m=bin(i)
-            count=Counter(m)
-            res.append(count["1"])
-        return res
+        dp = [0] * (n + 1)
+        sub = 1
+
+        for i in range(1, n + 1):
+            if sub * 2 == i:
+                sub = i
+            
+            dp[i] = dp[i - sub] + 1
+        
+        return dp
