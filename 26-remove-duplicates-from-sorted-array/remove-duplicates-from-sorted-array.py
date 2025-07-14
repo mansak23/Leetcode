532e20516b7e643508a8955e4b65011f8f877884
mansak23 <154529430+mansak23@users.noreply.github.com>
2025-07-15 00:03:48 +0530

    Time: 1 ms (60.18%) | Memory: 18.7 MB (94.59%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,3 +1,22 @@
+''''
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=0,j;
+        j=1;
+        //int flag=0;
+        while(j<nums.length && i<nums.length){
+            if(nums[i]==nums[j]){
+                j++;
+            }
+            else{
+                nums[i+1]=nums[j];
+                i++;
+            }
+        }
+        return i+1;
+    }
+}
+'''
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         n=list(set(nums))
