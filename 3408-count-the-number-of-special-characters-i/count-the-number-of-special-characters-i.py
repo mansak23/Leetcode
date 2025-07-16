1ae172a09cf8e2294dc41989a5cda6560a9b189d
mansak23 <154529430+mansak23@users.noreply.github.com>
2025-07-16 17:55:31 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (53.10%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.py
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.py
@@ -1,11 +1,16 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
+        # cnt=0
+        # visited=""
+        # for i in word:
+        #     if i.islower():
+        #         if (chr(ord(i)-32)in word) and (i not in visited):
+        #             print(i)
+        #             cnt+=1
+        #             visited+=i
+        # return cnt
         cnt=0
-        visited=""
-        for i in word:
-            if i.islower():
-                if (chr(ord(i)-32)in word) and (i not in visited):
-                    print(i)
-                    cnt+=1
-                    visited+=i
+        for i in set(word):
+            if i.islower() and (chr(ord(i)-32)in word):
+                cnt+=1
         return cnt
