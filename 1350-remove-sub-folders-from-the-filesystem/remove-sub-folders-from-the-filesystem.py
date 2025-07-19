8f8921579c55b3c07801fb5857cfc0da2f0fca17
mansak23 <154529430+mansak23@users.noreply.github.com>
2025-07-19 21:05:06 +0530

    Time: 27 ms (75.88%) | Memory: 30.5 MB (77.01%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,22 +1,34 @@
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        res=[]
-        if "/a/aaaaa" in folder:
-            return ["/a"]
         folder.sort()
-        res.append(folder[0])
-        j=0
-        for i in range(1,len(folder)):
-            if res[j] not in folder[i]:
-                res.append(folder[i])
-                j+=1
+        res = []
+        for f in folder:
+            if not res:
+                res.append(f)
             else:
-                if res[j][:2]!=folder[i][:2]:
-                    res.append(folder[i])
-                    j+=1
+                prev = res[-1]
+                if f.startswith(prev) and len(f) > len(prev) and f[len(prev)] == '/':
+                    continue
                 else:
-                    r=folder[i].replace(res[j],"")
-                    if r[0]!="/":
-                        res.append(folder[i])
-                        j+=1
+                    res.append(f)
         return res
+        # res=[]
+        # if "/a/aaaaa" in folder:
+        #     return ["/a"]
+        # folder.sort()
+        # res.append(folder[0])
+        # j=0
+        # for i in range(1,len(folder)):
+        #     if res[j] not in folder[i]:
+        #         res.append(folder[i])
+        #         j+=1
+        #     else:
+        #         if res[j][:2]!=folder[i][:2]:
+        #             res.append(folder[i])
+        #             j+=1
+        #         else:
+        #             r=folder[i].replace(res[j],"")
+        #             if r[0]!="/":
+        #                 res.append(folder[i])
+        #                 j+=1
+        # return res
