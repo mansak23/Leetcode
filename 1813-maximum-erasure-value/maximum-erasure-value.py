adc7cedc1151cab742e81781882464d336e4bf9a
Manasa K <154529430+mansak23@users.noreply.github.com>
2025-07-22 20:54:04 +0530

    Time: 199 ms (71.68%) | Memory: 28.8 MB (78.00%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.py
+++ 1813-maximum-erasure-value/maximum-erasure-value.py
@@ -1,29 +1,16 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
-    
-        seen = set()          # To store unique elements in current window
-        left = 0              # Left pointer of sliding window
-        curr_sum = 0          # Current sum of unique elements in window
-        max_sum = 0           # Store max sum
-        best_subarray = []    # To store the optimal subarray
-        current_subarray = [] # To track current subarray
-
-        for right in range(len(nums)):
-            while nums[right] in seen:
-            # Remove elements from the left until duplicate is gone
-                seen.remove(nums[left])
-                curr_sum -= nums[left]
-                current_subarray.pop(0)
-                left += 1
-
-            # Add new unique element
-            seen.add(nums[right])
-            curr_sum += nums[right]
-            current_subarray.append(nums[right])
-
-        # Update max sum and subarray if this is the best so far
-            if curr_sum > max_sum:
-                max_sum = curr_sum
-                best_subarray = current_subarray[:]
-
-        return max_sum
+        
+        seen=set()
+        s=0
+        l=0
+        mx=-1
+        for i in range(len(nums)):
+            while nums[i] in seen:
+                seen.remove(nums[l])
+                s-=nums[l]
+                l+=1
+            seen.add(nums[i])
+            s+=nums[i]
+            mx=max(mx,s)
+        return mx
