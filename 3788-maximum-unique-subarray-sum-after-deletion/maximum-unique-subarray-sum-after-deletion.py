04d27095f28976abbc589435f5d48c58ff98f15c
Manasa K <154529430+mansak23@users.noreply.github.com>
2025-07-25 13:42:50 +0530

    Time: 4 ms (10.77%) | Memory: 17.8 MB (30.68%) - LeetSync

--- 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.py
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.py
@@ -4,9 +4,7 @@ class Solution:
         nums=sorted(nums)
         total=sum(nums)
         for i in range (0,len(nums)):
-            print(nums[i:])
             sub=sum(nums[i:])
-            print(sub)
             if sub>total:
                 total = sub
         return total
