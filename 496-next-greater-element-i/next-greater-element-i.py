855e633dd59145071890a45b53cd292839c4eff5
Manasa K <154529430+mansak23@users.noreply.github.com>
2025-08-23 18:50:07 +0530

    Time: 21 ms (18.16%) | Memory: 17.8 MB (98.48%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,11 +1,9 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         res=[]
-        # flag=True
         for i in nums1:
             ind=nums2.index(i)
             flag=True
-            print(ind)
             for j in range(ind+1,len(nums2)):
                 if nums2[j]>i:
                     res.append(nums2[j])
@@ -13,6 +11,4 @@ class Solution:
                     break
             if flag:
                 res.append(-1)
-            print(res)
-        
         return res
