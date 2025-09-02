987fba2498792bcad7d4b5dd27521fb7261643bd
Manasa K <154529430+mansak23@users.noreply.github.com>
2025-09-02 23:07:46 +0530

    Time: 295 ms (7.98%) | Memory: 52.7 MB (69.95%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.py
+++ 354-russian-doll-envelopes/russian-doll-envelopes.py
@@ -1,22 +1,18 @@
 class Solution:
-    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
-        envelopes.sort(key=lambda x: (x[0], -x[1]))
-        
-        res = []		
-		# Perform LIS
-        for _, h in envelopes:
+    def maxEnvelopes(self, e: List[List[int]]) -> int:
+        e=sorted(e,key=lambda x:(x[0],-x[1]))
+        res=[]
+        for _,h in e:
             l,r=0,len(res)-1
-			# find the insertion point in the Sort order
-            while l <= r:
+            while l<=r:
                 mid=(l+r)>>1
                 if res[mid]>=h:
                     r=mid-1
                 else:
-                    l=mid+1        
-            idx = l
-            if idx == len(res):
+                    l=mid+1
+            ind=l
+            if ind==len(res):
                 res.append(h)
             else:
-                res[idx]=h
+                res[ind]=h
         return len(res)
-                                               
