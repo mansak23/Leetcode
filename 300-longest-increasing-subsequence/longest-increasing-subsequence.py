ba17955f60ab47e16bd7ba11a45a53bcf3042f43
Manasa K <154529430+mansak23@users.noreply.github.com>
2025-09-26 22:41:44 +0530

    Time: 3 ms (95.56%) | Memory: 17.9 MB (81.25%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sub=[]
+        for x in nums:
+            i=bisect_left(sub,x)
+            if i==len(sub):
+                sub.append(x)
+            else:
+                sub[i]=x
+        return len(sub)
