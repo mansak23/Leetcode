8e43d6a2ac084e2ec59a0c4772a9cb194ef48cac
Manasa K <154529430+mansak23@users.noreply.github.com>
2025-10-07 20:08:23 +0530

    Time: 7 ms (82.13%) | Memory: 19.5 MB (22.02%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,13 +1,5 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        str=re.sub(r'[^a-zA-Z0-9]','',s).lower()
-        print(str)
-        rev=str[::-1]
-        if(str==(rev)):
-            return True
-        return False
-        
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=re.sub(r'[^A-Za-z0-9]',"",s)
+        s=s.lower()
+        return s==s[::-1]
