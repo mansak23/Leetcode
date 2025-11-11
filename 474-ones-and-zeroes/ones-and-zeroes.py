d7542068d6f2c4b3e1b4050bb0df985e74851dba
Manasa K <154529430+mansak23@users.noreply.github.com>
2025-11-11 22:06:27 +0530

    Time: 1613 ms (49.96%) | Memory: 18 MB (95.64%) - LeetSync

---
+++ 474-ones-and-zeroes/ones-and-zeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMaxForm(self, strs, m, n):
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for s in strs:
+            z = s.count('0')
+            o = s.count('1')
+            for i in range(m, z-1, -1):
+                for j in range(n, o-1, -1):
+                    dp[i][j] = max(dp[i][j], dp[i - z][j - o] + 1)
+        return dp[m][n]
